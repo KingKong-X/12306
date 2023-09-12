7c3c4651ac825b36f5dfac0d1b2dc79348ab8038
xingyuan <huang.xingyuan@outlook.com>
2023-09-12 09:09:27 +0800

    Project Initialize

---
+++ service/user-service/src/main/java/com/xingyuan/study/userservice/controller/UserLoginController.java
@@ -0,0 +1,15 @@
+package com.xingyuan.study.userservice.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Xingyuan Huang
+ * @since 2023/9/11 20:48
+ */
+@RestController
+@RequiredArgsConstructor
+public class UserLoginController {
+
+}
